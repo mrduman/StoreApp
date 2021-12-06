71c26c145d0a9e67b377a9e851f9a419e72aba18
mrduman <omer.duman@gmx.de>
2021-12-06 11:03:24 +0100

    formik

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,11 +2,15 @@ import React from "react";
 import { View, TextInput } from "react-native";
 import styles from './Input.style'
 
-export default function Input({placeholder , onChangeText}) {
+export default function Input({placeholder , onChangeText, value}) {
 
     return(
         <View style = {styles.container}>
-            <TextInput placeholder = {placeholder} onChangeText = {onChangeText} />
+            <TextInput
+             placeholder = {placeholder} 
+             onChangeText = {onChangeText} 
+             value = {value}
+             />
         </View>
     )
     
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,21 +3,41 @@ import { View, Text, Image } from "react-native";
 import Button from "../../components/Button/Button";
 import Input from "../../components/Input";
 import styles from './Login.style'
+import { Formik } from 'formik'
 
 export default function Login() {
+
+    function handleLogin(values) {
+        console.log(values)
+    }
 
     return (
         <View style={styles.container}>
             <View style={styles.logo_container}>
                 <Image style={styles.image} source={require('../../asstes/logo-icon.png')} />
             </View>
-            <View style={styles.body_container}>
-                <Input placeholder='Text to Username' />
-                <Input placeholder='Text to Password' />
-                <Button text='Login' />
 
-                <Text></Text>
-            </View>
+            <Formik
+              initialValues = {{username : '' , password : ''}} 
+              onSubmit = {handleLogin}
+            >
+                
+              {({handleChange,handleSubmit,values}) =>  (<View style={styles.body_container}>
+
+                    <Input 
+                    placeholder='Text to Username' 
+                    value = {values.username}
+                    onChangeText = {handleChange('username')}
+                    />
+                    <Input 
+                    placeholder='Text to Password'
+                     value = {values.password}
+                     onChangeText = {handleChange('password')}
+                    />
+
+                    <Button text='Login' onPress = {handleSubmit} />
+                </View>)}
+            </Formik>
         </View>
     )
 
